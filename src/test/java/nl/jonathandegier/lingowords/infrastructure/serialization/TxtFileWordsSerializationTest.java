9879af7c80f04be06385da82f5155ca84eaf950f
Jonathan <jonathan.degier@student.hu.nl>
2020-05-14 17:25:36 +0200

    fix tests

--- src/test/java/nl/jonathandegier/lingowords/infrastructure/serialization/TxtFileWordsSerializationTest.java
+++ src/test/java/nl/jonathandegier/lingowords/infrastructure/serialization/TxtFileWordsSerializationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import util.FileContent;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
