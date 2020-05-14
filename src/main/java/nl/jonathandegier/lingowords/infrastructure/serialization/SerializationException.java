caf79109cd75344677ec93c46da999047e6d9068
Jonathan <jonathan.degier@student.hu.nl>
2020-05-14 17:11:10 +0200

    removed all code smells

---
+++ src/main/java/nl/jonathandegier/lingowords/infrastructure/serialization/SerializationException.java
@@ -0,0 +1,8 @@
+package nl.jonathandegier.lingowords.infrastructure.serialization;
+
+public class SerializationException extends RuntimeException {
+
+    public SerializationException(String message) {
+        super(message);
+    }
+}
